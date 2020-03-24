3883539eb86d8667cbeda79d44e4d3b0fc80f23b
Jiajun Yan <jiajun.yan@outlook.com>
2020-03-24 17:47:00 +0800

    Complete example

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header';
+
+export default function Index() {
+	return (
+		<div>
+            <Header></Header>
+			<p>this is about next.js</p>
+		</div>
+	);
+}
